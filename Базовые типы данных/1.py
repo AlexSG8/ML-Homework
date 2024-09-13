6557273b2653ae88e83f89e337cba8da28d150d3
AlexSG8 <46405594+AlexSG8@users.noreply.github.com>
2024-09-13 10:00:52 +0400

    Update 1.py

--- Базовые типы данных/1.py
+++ Базовые типы данных/1.py
@@ -1,2 +1,10 @@
+print("Enter 5 digit:")
 in_digit = input()
-print("Output:", int(in_digit[0]+in_digit[3]+in_digit[2]+in_digit[1]+in_digit[-1]))
+
+while len(in_digit) != 5:
+    print("Enter 5 digit:")
+    in_digit = input()
+
+out = int(in_digit[0]+in_digit[3]+in_digit[2]+in_digit[1]+in_digit[-1])
+
+print("Output:", out)
